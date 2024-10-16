b9472c33c82af4cd8e04ff16e0ea3c377de2d6d3
Filipe Cabaço <filipecabaco@gmail.com>
2024-10-16 13:45:25 +0100

    temporarly remove mistral

--- lib/skyfall/models.ex
+++ lib/skyfall/models.ex
@@ -4,7 +4,7 @@ defmodule Skyfall.Models do
   def models(),
     do: [
       %__MODULE__{name: :gemma, repository: "google/gemma-2b", auth?: true, tensor_type: :bf16},
-      %__MODULE__{name: :phi, repository: "microsoft/Phi-3.5-mini-instruct", auth?: false, tensor_type: :bf16},
-      %__MODULE__{name: :nemo, repository: "mistralai/Mistral-Nemo-Instruct-2407", auth?: true, tensor_type: :bf16}
+      %__MODULE__{name: :phi, repository: "microsoft/Phi-3.5-mini-instruct", auth?: false, tensor_type: :bf16}
+      # %__MODULE__{name: :nemo, repository: "mistralai/Mistral-Nemo-Instruct-2407", auth?: true, tensor_type: :bf16}
     ]
 end
